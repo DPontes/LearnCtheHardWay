f43d5916c8a1ac23793d376fc302ba32046f824b
DPontes <dpontes@protonmail.com>
2020-01-15 13:12:57 +0100

    added /*@unused@*/ to comply with splint

--- ex1.c
+++ ex1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int /*@unused@*/argc, char /*@unused@*/*argv[]) {
     int distance = 100;
     printf("You are %d kilometers away\n", distance);
 

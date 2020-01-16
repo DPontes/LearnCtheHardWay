5c546a929d70ed36f90d76cb9819aacc2cbf44bd
DPontes <dpontes@protonmail.com>
2020-01-16 13:58:58 +0100

    Added extra credit function that capitalizes the letter

--- ex10.c
+++ ex10.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+// Extra credit, make a math based function that capitalizes the input
+// so that the extra 'case' in the switch are removed
+char capitalize(char letter) {
+    if (letter >= 'a' && letter <= 'z') {
+        letter -= 32;
+    }
+    return letter;
+}
+
 int main(int argc, char *argv[]) {
+
     if(argc != 2) {
         printf("ERROR! You must input exactly one argument.\n");
         // this is how you abort a program
@@ -10,29 +20,24 @@ int main(int argc, char *argv[]) {
     int i = 0;
     for(i = 0; argv[1][i] != '\0'; i++) {
         char letter = argv[1][i];
+        letter = capitalize(letter);
 
         switch (letter) {
-            case 'a': // fallthrough
             case 'A':
                 printf("%d: 'A'\n", i);
                 break;
-            case 'e': // fallthrough
             case 'E':
                 printf("%d: 'E'\n", i);
                 break;
-            case 'i': // fallthrough
             case 'I':
                 printf("%d: 'I'\n", i);
                 break;
-            case 'o': // fallthrough
             case 'O':
                 printf("%d: 'O'\n", i);
                 break;
-            case 'u': // fallthrough
             case 'U':
                 printf("%d: 'U'\n", i);
                 break;
-            case 'y': // fallthrough
             case 'Y':
                 // why i > 2? is this a bug?
                 if(i > 2) {
